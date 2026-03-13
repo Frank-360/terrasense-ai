af34cb656f796ca375f6b37a609ba870152ee6a3
Frank Esogwa <64640036+Frank-360@users.noreply.github.com>
2026-03-13 14:00:19 +0100

    Update terrasense-ai.py

--- app/terrasense-ai.py
+++ app/terrasense-ai.py
@@ -7,6 +7,8 @@ from streamlit_geolocation import streamlit_geolocation
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 from io import BytesIO
+from auth.login import login_user
+from auth.register import register_user
 
 st.set_page_config(
     page_title="TerraSense AI",
