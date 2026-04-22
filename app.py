ebe34eb2209b4ce4e51e5f80624fa9331dbb4fe5
Frank Esogwa <frank_esogwa@yahoo.com>
2026-04-22 14:35:27 +0100

    Enable CORS for frontend access

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, request, jsonify
 import requests
 import random
 import datetime
+from flask_cors import CORS
 
+
+app = Flask(__name__)
+CORS(app)
 app = Flask(__name__)
 
 # ---------------------------
